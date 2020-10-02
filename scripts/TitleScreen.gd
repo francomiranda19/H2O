695c28292c69a3f3aed871a29d2a7948746c55d3
Vicho21 <vicho21999@gmail.com>
2020-10-02 01:27:43 -0300

    Agregada pantalla de controles e interfaz de vida

--- scripts/TitleScreen.gd
+++ scripts/TitleScreen.gd
@@ -3,12 +3,17 @@ extends CanvasLayer
 
 func _ready():
 	$Panel/VBoxContainer/Start.connect("pressed",self,"on_Start_pressed")
+	$Panel/VBoxContainer/Como_jugar.connect("pressed",self,"on_Controls_pressed")
 	$Panel/VBoxContainer/Exit.connect("pressed",self,"on_Exit_pressed")
 	
 func on_Start_pressed():
 	$Click.play()
 	LevelManager.next()
 	
+func on_Controls_pressed():
+	$Click.play()
+	get_tree().change_scene("res://scene/Controls.tscn")
+
 func on_Exit_pressed():
 	$Click.play()
 	get_tree().quit()
