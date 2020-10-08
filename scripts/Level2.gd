bffc28ec91dfbdb172078702f7a4f60ba22b5bed
Vicho21 <vicho21999@gmail.com>
2020-10-07 23:34:59 -0300

    Se agregan checkpoints a nivel 2

--- scripts/Level2.gd
+++ scripts/Level2.gd
@@ -1,4 +1,15 @@
 extends Node2D
 
+onready var current_checkpoint = $Node2D.get_child(0) 
+
 func _ready():
 	$Rain.play("rain")
+	for checkpoint in $Node2D.get_children():
+		checkpoint.connect("player_entered", self, "on_player_entered")
+		
+func on_player_entered(checkpoint):
+	current_checkpoint = checkpoint
+
+func teleport_checkpoint(player):
+	player.global_position = current_checkpoint.get_node("Spawn").global_position
+		 
